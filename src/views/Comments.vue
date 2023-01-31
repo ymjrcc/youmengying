819b109f830c4ab8a469a063aa7877f92114c2f9
Yiming <ymjrcc@qq.com>
2023-01-31 15:21:59 +0800

    优化

--- src/views/Comments.vue
+++ src/views/Comments.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import {ref} from 'vue'
+import { RouterLink } from 'vue-router'
 import comments from '@/assets/data/comments.json'
 
 interface IComment {
@@ -30,17 +31,24 @@ const handleClick = (item: IComment) => {
       </ul>
     </div>
     <div class="flex-1" v-if="curComment && curComment.name">
-      <div class="text-3xl mb-4">
-        {{ curComment.name }}
-      </div>
-      <div>
-        {{ curComment.hometown }}
-      </div>
-      <div>
-        {{ curComment.brief }}
-      </div>
-      <div>
-        {{ curComment.list }}
+      <div class="text-3xl mb-4">{{ curComment.name }}</div>
+      <div class="text-gray-500 mb-2">【籍贯/常住地】</div>
+      <div class="indent-lg mb-4">{{ curComment.hometown }}</div>
+      <div class="text-gray-500 mb-2">【简介】</div>
+      <div class="indent-lg mb-4">{{ curComment.brief }}</div>
+      <div class="text-gray-500 mb-2">【原文】</div>
+      <div class="indent-lg pb-4">
+        <div v-for="id in curComment.list" :key="id" class="mb-1">
+          <RouterLink :to="`/original?id=${id}`" class="text-green-500 underline">
+            <span v-if="id===-1">序一</span>
+            <span v-else-if="id===-2">序二</span>
+            <span v-else-if="id===-3">序三</span>
+            <span v-else-if="id===220">跋一</span>
+            <span v-else-if="id===221">跋二</span>
+            <span v-else-if="id===222">跋三</span>
+            <span v-else>第 {{ id }} 则</span>
+          </RouterLink>
+        </div>
       </div>
     </div>
   </div>
@@ -49,5 +57,6 @@ const handleClick = (item: IComment) => {
 <style lang="less" scoped>
 .cur{
   background-color: #a7f3d0;
+  font-weight: bold;
 }
 </style>
--- src/views/Original.vue
+++ src/views/Original.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import type { Ref } from 'vue';
+import { useRoute } from 'vue-router'
 import xu from '@/assets/data/xu.json'
 import original from '@/assets/data/original.json'
 
 interface IXu {
-  id: string;
+  id: number;
   title: string;
   author: string;
   text: string[];
@@ -20,8 +21,28 @@ interface IOriginal {
     }[];
 }
 
-const curXu: Ref<IXu | null> = ref(xu[0])
+const curXu: Ref<IXu | null> = ref(null)
 const curOriginal: Ref<IOriginal | null> = ref(null)
+
+const route = useRoute()
+
+onMounted(() => {
+  if(!route.query || !route.query.id){
+    curXu.value = xu[0]
+    return
+  }
+  if(Number(route.query.id) > 0){
+    const value = original.find(i => i.id===Number(route.query.id))
+    if(value){
+      curOriginal.value = value
+    }
+  }else{
+    const value = xu.find(i => i.id===Number(route.query.id))
+    if(value){
+      curXu.value = value
+    }
+  }
+})
 
 const handleClick = (item: IXu | IOriginal, type: number) => {
   if(type===1){
@@ -35,23 +56,28 @@ const handleClick = (item: IXu | IOriginal, type: number) => {
 </script>
 
 <template>
-  <div class="flex">
-    <div class="w-50">
-      <ul class="border mr-10">
+  <div class="flex h-full">
+    <div class="w-60">
+      <ul class="border mr-10 h-full overflow-auto">
         <li 
           v-for="item in xu" 
           :key="item.id" 
-          class="py-2 px-4 cursor-pointer hover:bg-green-100"
+          class="py-2 px-6 cursor-pointer hover:bg-green-100"
           :class="{cur: item.id===(curXu && curXu.id)}"
           @click="handleClick(item, 1)"
         >{{ item.title }}</li>
         <li 
           v-for="item in original" 
           :key="item.id" 
-          class="py-2 px-4 cursor-pointer hover:bg-green-100"
+          class="py-2 px-6 cursor-pointer hover:bg-green-100"
           :class="{cur: item.id===(curOriginal && curOriginal.id)}"
           @click="handleClick(item, 2)"
-        >第{{ item.id }}则</li>
+        >
+          <span v-if="item.id===220">跋一</span>
+          <span v-else-if="item.id===221">跋一</span>
+          <span v-else-if="item.id===222">跋一</span>
+          <span v-else>第{{ item.id }}则</span>
+        </li>
       </ul>
     </div>
     <div class="flex-1" v-if="curXu">
@@ -72,5 +98,6 @@ const handleClick = (item: IXu | IOriginal, type: number) => {
 <style lang="less" scoped>
 .cur{
   background-color: #a7f3d0;
+  font-weight: bold;
 }
 </style>
