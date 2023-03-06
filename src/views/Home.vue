5cb49392937e2edaf7c296ade114cfc52f7091a1
heyiming <ymjrcc@qq.com>
2023-03-06 16:27:27 +0800

    搜索

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import {NInputGroup, NInput, NButton} from 'naive-ui'
+
+const router = useRouter()
+
+const inputValue = ref('')
+
+const handleSearch = () => {
+  if(!inputValue.value) return
+  router.push({path: '/search', query: {keyword: inputValue.value}})
+}
 </script>
 
 <template>
@@ -7,8 +18,8 @@ import {NInputGroup, NInput, NButton} from 'naive-ui'
     <h1 class="text-5xl text-center text-gray-500 font-bold mt-20 mb-15">《幽梦影》数字平台</h1>
     <div class="mx-auto w-100">
       <n-input-group>
-        <n-input placeholder="请输入关键词" size="large"/>
-        <n-button type="primary" size="large">
+        <n-input v-model:value="inputValue" clearable placeholder="请输入关键词" size="large" @change="handleSearch"/>
+        <n-button type="primary" size="large" @click="handleSearch">
           搜索
         </n-button>
       </n-input-group>
